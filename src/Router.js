af1f4e8a8a0a65b89fa460d9a4ec2a7a423c9536
Barış Kandemir <48553738+bariskndmr@users.noreply.github.com>
2022-01-08 20:41:21 +0300

    esl

--- src/Router.js
+++ src/Router.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
